4371de92167b997a63dac07538a8080ba7f3e8c5
faithmwamkinga <faithmwamkinga@gmail.com>
2023-04-26 22:56:40 +0300

    objects

--- objects.js
+++ objects.js
@@ -2,15 +2,24 @@
 // name and age property, write a function that returns a new array
 // containing the names of all people who are 18 years old or older.
 
-function peopleNames(people) {
-  const adults=[];
-  for (i=0;i<people.length;i++){
-    const onePerson= people[i];
-    if(people.age>=18){
-      adults.push(onePerson.name)
-    }
-  }
-   return adults;
+// function peopleNames(people) {
+//   const adults=[];
+//   for (i=0;i<people.length;i++){
+//     const onePerson= people;
+//     if(people.age>=18){
+//       adults.push(onePerson.name)
+//     }
+//   }
+//    return adults;
+// }
+// console.log(adults)
+
+function oldNames(){
+  let olderPeolple = people.filter(
+    person=>person.age>=18
+  )
+  let namesAbove=olderPeolple.map(person=>person.name)
+  return namesAbove
 }
 const people = [
     { name: 'Alice', age: 17 },
@@ -18,10 +27,8 @@ const people = [
     { name: 'Charlie', age: 14 },
     { name: 'Max', age: 19 },
  ];
+console.log(oldNames());
 
-const adults = peopleNames(people);
-console.log(adults);
-peopleNames()
 
 
 // Write a function that takes an array of objects, where each object represents a product with a name, price, and category property.
@@ -35,53 +42,54 @@ peopleNames()
 //   ];
 
 
-function myProducts(materials) {
-    const aProduct = {};
-    for (let i = 0; i < materials.length; i++) {
-      const aProduct = myProducts[i].products;
-      if (myProducts[aProduct]) {
-        myProducts[aProduct].push(products[i]);
-      } else {
-        myProducts[aProduct] = [products[i]];
-      }
-    }
-    return myProducts;
-  }
-  const products = [
-    { name: 'Laptop', price: 1200, category: 'Electronics' },
-    { name: 'Shirt', price: 25, category: 'Clothing' },
-    { name: 'Headphones', price: 80, category: 'Electronics' },
-    { name: 'Shoes', price: 60, category: 'Clothing' },
-  ];
-  const productsByCategory = myProducts(products);
-console.log(productsByCategory);
+// function myProducts(materials) {
+//     const aProduct = {};
+//     for (let i = 0; i < materials.length; i++) {
+//       const aProduct = myProducts[i].products;
+//       if (myProducts[aProduct]) {
+//         myProducts[aProduct].push(products[i]);
+//       } else {
+//         myProducts[aProduct] = [products[i]];
+//       }
+//     }
+//     return myProducts;
+//   }
+//   const products = [
+//     { name: 'Laptop', price: 1200, category: 'Electronics' },
+//     { name: 'Shirt', price: 25, category: 'Clothing' },
+//     { name: 'Headphones', price: 80, category: 'Electronics' },
+//     { name: 'Shoes', price: 60, category: 'Clothing' },
+//   ];
+//   const productsByCategory = myProducts(products);
+// console.log(productsByCategory);
 
 // Given an array of objects, where each object represents a student
 // with a name and an array of scores, write a function that returns a 
 //new array containing the names of all students whose average score 
 //is greater than or equal to 85.
 
-function student(students){
-  let newStudent=[];
-  let total=students.scores+students.scores+students.scores
-  let studentAverage=total/students.scores
-  for (stud in students){
-    if (studentAverage>=85){
-      newStudent.push(students.name)
-      
+function excelStudent(students){
+  let passStudent=[];
+  students.forEach(student=>{
+    const{name,scores}=student
 
-    }
-    console.log(newStudent)
+    const averages=scores.reduce((sum,scores)=>sum+scores,0)/scores.length;
+  if (averages>=85){
+   passStudent.push(name);
   }
 
+
+});
+return passStudent
 }
+
 const students = [
   { name: 'John', scores: [90, 80, 85] },
   { name: 'Jane', scores: [95, 92, 88] },
   { name: 'Jim', scores: [70, 80, 75] },
   { name: 'Jill', scores: [85, 90, 84] },
 ];
-student(students)
+console.log(excelStudent(students))
 
 
 //Given an object representing a car, with properties for the make, model, year, and a method to
@@ -91,14 +99,16 @@ student(students)
 
 
 function myCar(){
-  
-  car.age=20; 
+  let yearNow=new Date().getFullYear();
+  car.age=function(){
+    return yearNow(this.year);
+  } 
 }
 const car = {
   make: 'Ford',
   model: 'Ranger',
   year: 2023,
-  displayInfo: function() {
-    console.log(`Make: ${this.make}, Model: ${this.model}, Year: ${this.year}`);
-  },
-};
+}
+
+console.log(`Make: ${this.make}, Model: ${this.model}, Year: ${this.year}`);
+myCar()
